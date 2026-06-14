e498605a1ed41757420639b0d5b0552b343f0fbf
vishalkhetan24 <vishalkhetan24@gmail.com>
2026-06-15 01:45:32 +0530

    Untrack generated Vercel build output and ignore .vercel directory

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DLtNZ5EX.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/shravan rawas/Desktop/Projects/Personal React Projects/sakshi-abhishek-wedding-invitation/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-WDZWG-1w.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-WDZWG-1w.js" } }] }, "/": { filePath: "C:/Users/shravan rawas/Desktop/Projects/Personal React Projects/sakshi-abhishek-wedding-invitation/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BEl0YJlY.js"] } } });
-export {
-  tsrStartManifest
-};
